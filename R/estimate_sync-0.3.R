6cafe99fcb6dd776b500a93ef3d7d142c4171693
Filippo Ferrario <filippo.f3rrario@gmail.com>
2023-05-05 10:16:00 -0400

    updated terminology for camera role
    Camera roles master and slave have been amended to be primary and secondary, in an attempt to use a less discriminatory language.

--- R/estimate_sync-0.3.R
+++ R/estimate_sync-0.3.R
@@ -1,8 +1,8 @@
 # ===============================================================================
 # Name   	: Estimate imagery syncronization
 # Author 	: Filippo Ferrario
-# Date   	: 18-10-2022 [dd-mm-yyyy]
-# Version	: 0.3
+# Date   	: 18-10-2022 [dd-mm-yyyy] | 05-05-2023
+# Version	: 0.4
 # URL		: 
 # Aim    	: Given a set of reference points in a sequence of images taken at regular intervals (i.e., the file name of an image corresponding to a known scene, such as the beginning or the end of a chunk of images of interest),
 #			  the function should estimate the file name of corresponding images in a different image sequence taken at the same regular interval.
@@ -18,16 +18,20 @@
 #'			   
 #'
 #' @param source absolute or relative path to the folder/drive where the subfolders with the image files of the individual cameras are stored. 
-#' @param ref_data dataframe with information about reference points in the sequence for Master and initial ref points for slaves.
+#' @param ref_data dataframe with information about reference points in the sequence for primary camera and initial ref points for secondaries.
 #' @param output_path absolute or relative path to the output file (.csv). It must end with the file name
 #' @param shooting_group name of the column in the dataset specifying the group of picture taken in the same shooting session 
 #' @param cameraID name of the column in the dataset grouping the picture by camera 
-#' @param camera_role name of the column in the dataset specifying the role of the camera. That is 'master' for the camera used to retrive reference points, 'slave' for the other cameras that need to be synchronized with the 'master'
+#' @param camera_role name of the column in the dataset specifying the role of the camera. That is 'primary' for the camera used to retrive reference points, 'secondary' for the other cameras that need to be synchronized with the 'master'
 #' @param sequence name of the column in the dataset grouping the images into a continuous sequence within the shooting session (e.g., a transect from start to end)
 #' @param ref_point name of the column in the dataset specifying if the reference point is the 'start' or the 'end' of the sequence 
 #' @param file_name name of the column in the dataset specifying the file name of the image taken as reference point
-#' @param img_extension character specifying the file format of the images to use. DEFAUL to 'JPG'. it is case insensitive.
+#' @param img_extension character specifying the file format of the images to use. DEFAULT to 'JPG'. it is case insensitive.
 #' 
+#' @details
+#' 
+#' For compatibility with previuous versions the `camera_role` terminology can be 'master' and 'slave' for 'primary' and 'secondary' respectively. 
+#' However these terms will be changed in the function and in its output to the 'primary' and 'secondary'.  
 #' 
 #' @author Filippo Ferrario, \email{filippo.f3rrario@gmail.com} 
 #' 
@@ -39,7 +43,7 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 {		
 		# check args
 		if (class(ref_data)!='data.frame' | is.null(ref_data)) stop('ref_data must be a data.frame')
-		if (sum(ref_data[,camera_role] %in% c('master', 'slave'))!= length(ref_data[,camera_role])) stop('camera_role in ref_data must be either "master" or "slave"')
+		if (sum(ref_data[,camera_role] %in% c('master', 'slave','primary','secondary'))!= length(ref_data[,camera_role])) stop('camera_role in ref_data must be either "primary" or "secondary"')
 		if (sum(!ref_data[,ref_point] %in% c('start', 'end'))>0) stop('ref_point in ref_data must be either "start" or "end"')
 		if (!is.character(source)) stop('source must be a character')
 		if (!is.character(output_path)) stop('output_path must be a character')
@@ -57,6 +61,9 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 		# # create unique annotations
 		# ref_data$annot<-paste(ref_data[,camera_role],ref_data[,cameraID],ref_data[,sequence],ref_data[,ref_point], sep=' ' ) 
 		
+		# change master and slaves to Primary and secondary roles 
+		ref_data$camera_role<- gsub(ref_data$camera_role, pattern='master', replacement='primary')
+		ref_data$camera_role<- gsub(ref_data$camera_role, pattern='slave', replacement='secondary') 
 ########
 		comps<- strsplit(rel_path, split='/')
 		f<-unlist(lapply(comps ,function(x){ #browser()
@@ -71,11 +78,11 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 		# ID_role<-paste0(ref_data[,cameraID],ref_data[,camera_role])
 		ID_role<-paste(ref_data[,shooting_group],ref_data[,cameraID],ref_data[,camera_role], ref_data[,sequence])
 		uni_ID_role<-unique(ID_role)
-		masters<-grep(uni_ID_role, pattern='master', value=TRUE)
-		slaves<-grep(uni_ID_role, pattern='slave', value=TRUE)
+		masters<-grep(uni_ID_role, pattern='primary', value=TRUE)
+		slaves<-grep(uni_ID_role, pattern='secondary', value=TRUE)
 		
 		def_slaves<-lapply(masters, function(x) { #browser()
-										ms_cam<-unlist(strsplit(x, split='master'))
+										ms_cam<-unlist(strsplit(x, split='primary'))
 										# shoot_str<-regexpr(ms_cam[1], pattern='[[:alnum:]_-]+')
 										shoot_str<-regexpr(ms_cam[1], pattern='(.+? )')
 										shoot_gpr<-regmatches(ms_cam[1],m=shoot_str)
@@ -83,23 +90,23 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 
 										# sl_cam<-slaves[-grep(slaves,pattern=ms_cam)]
 										# sl_cam<-slaves[grep(slaves,pattern=ms_cam[2])]
-										sl_cam<-slaves[grep(slaves,pattern=paste0(shoot_gpr,'(.+?) slave',ms_cam[2]))]
+										sl_cam<-slaves[grep(slaves,pattern=paste0(shoot_gpr,'(.+?) secondary',ms_cam[2]))]
 										sl_cam
 										})
 		names(def_slaves)<-masters
 		def_slaves<-def_slaves[!lengths(def_slaves)==0]
-		if(length(unique(lengths(def_slaves)))>1) stop('Number of slaves are not equal for all masters. Double check input data.')
+		if(length(unique(lengths(def_slaves)))>1) stop('Number of secondaries are not equal for all masters. Double check input data.')
 
 		# order ref_data to make sure to define the correct pairing of starts and ends refpoint in a sequence.
 
 		ref_data_split<-split(ref_data, f=paste(ref_data[,shooting_group], ref_data[,cameraID],ref_data[,camera_role]))
 		ord_ref_data_split<-lapply(ref_data_split, function(x) x[order(x[,file_name]),])
-		ref_data_master<-ord_ref_data_split[grepl(ord_ref_data_split, pattern='master')]
+		ref_data_master<-ord_ref_data_split[grepl(ord_ref_data_split, pattern='primary')]
 		
 
 		# isolate the starting points for salves
-		# ref_data_slave<-ord_ref_data_split[grepl(ord_ref_data_split, pattern='slave')]
-		ref_data_slave<-ref_data[ref_data[camera_role]=='slave',]
+		# ref_data_slave<-ord_ref_data_split[grepl(ord_ref_data_split, pattern='secondary')]
+		ref_data_slave<-ref_data[ref_data[camera_role]=='secondary',]
 		ref_data_slave<-split(ref_data_slave, f=paste(ref_data_slave[,shooting_group], ref_data_slave[,cameraID],ref_data_slave[,camera_role],ref_data_slave[,sequence]))
 
 
@@ -109,8 +116,8 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 		
 
 		# define lengths intervals of files in master(s)
-		master_starts<-lapply(ref_data_master, function(x) x[ x[,camera_role] =='master' & x[,ref_point] =='start',]	)
-		master_ends<-lapply(ref_data_master, function(x)x[ x[,camera_role] =='master' & x[,ref_point] =='end',]	)
+		master_starts<-lapply(ref_data_master, function(x) x[ x[,camera_role] =='primary' & x[,ref_point] =='start',]	)
+		master_ends<-lapply(ref_data_master, function(x)x[ x[,camera_role] =='primary' & x[,ref_point] =='end',]	)
 		
 		mst_start_pathID<-lapply(master_starts, function(x) grep(rel_path, pattern=paste0(x[,file_name], collapse='|')))
 		mst_end_pathID<-lapply(master_ends, function(x) grep(rel_path, pattern=paste0(x[,file_name], collapse='|')))
@@ -139,7 +146,7 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 		)
 
 
-		# estimate reference points for slaves
+		# estimate reference points for secondaries
 		lsnam<-names(mst_seq_len) 
 
 		# lsnam<-lsnam[2]
@@ -149,11 +156,11 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 		 	slvs<-def_slaves[[ grep(names(def_slaves), pattern=x) ]]
 		 	
 		 	# error check
-		 	if (sum(lengths(ref_data_slave[slvs])==0)>0) stop('Unspecified slaves: For each master, make sure to have defined the first starts point of each slave!')
-		 	# estimate reference points from starting position of each slave and save it as a dataframe list
+		 	if (sum(lengths(ref_data_slave[slvs])==0)>0) stop('Unspecified secondaries: For each primary, make sure to have defined the first starts point of each secondary!')
+		 	# estimate reference points from starting position of each secondary and save it as a dataframe list
 		 	out2<-lapply(ref_data_slave[slvs] , function(k) { #browser()
 ############		 	
-		 			# find folder of the slave
+		 			# find folder of the secondary
 		 			folder<- lapply(path_list, function(f){
 		 					sum(grepl(f,pattern=k[,file_name]))
 		 				})
@@ -162,12 +169,12 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 ############
 		 			# file position of start ref point
 		 			pathID<-grep(fold_paths, pattern=k[,file_name] )
-		 			# lenght of good and junk sequences for the master in use
+		 			# lenght of good and junk sequences for the primary in use
 		 			seq_len<-unlist(mst_seq_len[x])
 		 			jnks<-unlist(mst_junk_len[x])
 					new_ref<-NULL
 					tmp_start<-pathID
-					# find pathID for new start and ends for a slave
+					# find pathID for new start and ends for a secondary
 		 			for (i in 1:length(seq_len)){
 		 							temp_ref1<-tmp_start+seq_len[i]
 		 							if (i<length(seq_len)){
@@ -182,9 +189,9 @@ estimate_sync<-function(source=NULL, ref_data=NULL, output_path=NULL,shooting_gr
 
 #######
 					# check if for each reference point exist a picture
-					if(anyNA(fold_paths[new_ref])) stop(paste0('Some slave starting point is to far from its master: not enough pics for each sequence for camera ',names(folderID),'.\n Double check starting points for slaves.'))
+					if(anyNA(fold_paths[new_ref])) stop(paste0('Some secondary starting point is to far from its primary: not enough pics for each sequence for camera ',names(folderID),'.\n Double check starting points for secondaries.'))
 #######		 									
-		 			# initialize dataframe output using master dataframe as template						
+		 			# initialize dataframe output using primary dataframe as template						
 		 			out<- ord_ref_data_split[x][[1]]
 		 			out<-out[2:nrow(out),]
 		 			# find files of reference points
